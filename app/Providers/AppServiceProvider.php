9117cd481539c843cb7bafd10f456e82fa5f459c
João Dal Lago <joaogabrieldallago@hotmail.com>
2024-02-17 19:45:11 -0300

    :bug: fix: Correção do bind dos repositórios
    Adiciona bind das interfaces com classes de implementação dos repositórios.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,18 @@
 
 namespace App\Providers;
 
+use App\Repositories\BaseRepository;
+use App\Repositories\CartItemRepository;
+use App\Repositories\CartRepository;
+use App\Repositories\ProductRepository;
+use App\Repositories\PromotionRepository;
+use App\Repositories\RuleRepository;
+use App\RepositoryInterfaces\BaseRepositoryInterface;
+use App\RepositoryInterfaces\CartItemRepositoryInterface;
+use App\RepositoryInterfaces\CartRepositoryInterface;
+use App\RepositoryInterfaces\ProductRepositoryInterface;
+use App\RepositoryInterfaces\PromotionRepositoryInterface;
+use App\RepositoryInterfaces\RuleRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(BaseRepositoryInterface::class, BaseRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+        $this->app->bind(CartItemRepositoryInterface::class, CartItemRepository::class);
+        $this->app->bind(CartRepositoryInterface::class, CartRepository::class);
+        $this->app->bind(PromotionRepositoryInterface::class, PromotionRepository::class);
+        $this->app->bind(RuleRepositoryInterface::class, RuleRepository::class);
+
     }
 
     /**
