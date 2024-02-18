7847de42b04b9f3f51a3c0602ccf1f6ddd85d6e4
João Dal Lago <joaogabrieldallago@hotmail.com>
2024-02-18 17:58:33 -0300

    :sparkles: feat: links do menu
    Adiciona links de navegação.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,18 +4,31 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
+{{--                <div class="shrink-0 flex items-center">--}}
+{{--                    <a href="{{ route('dashboard') }}">--}}
+{{--                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />--}}
+{{--                    </a>--}}
+{{--                </div>--}}
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('products.index')" :active="request()->routeIs('products.index')">
+                        {{ __('Produtos') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('carts.index')" :active="request()->routeIs('carts.index')">
+                        {{ __('Meu carrinho') }}
+                    </x-nav-link>
+
+{{--                    <x-nav-link :href="route('promotions.index')" :active="request()->routeIs('promotions.index')">--}}
+{{--                        {{ __('Promoções') }}--}}
+{{--                    </x-nav-link>--}}
                 </div>
+
             </div>
 
             <!-- Settings Dropdown -->
