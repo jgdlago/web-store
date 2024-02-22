29659dd392833bd86933b8ce730e8521dfad0c64
João Dal Lago <50213926+jgdlago@users.noreply.github.com>
2024-02-22 03:00:01 -0300

    :sparkles: Feat: Cria seeder de dados
    Adiciona seeders para as tabelas de Produtcts, Promotions e Rules.

---
+++ database/seeders/RuleSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Rule;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RuleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $rules = [
+            [
+              'name' => 'Compre 1 ganhe outro',
+              'buy_quantity' => 1,
+              'get_quantity' => 1
+            ],
+            [
+                'name' => 'Mais de 1 un 21,70',
+                'minimum_quantity' => 1,
+                'promotion_price' => 2170,
+            ],
+            [
+                'name' => 'Compre 1 fardo 7,50 un',
+                'minimum_quantity' => 6,
+                'promotion_price' => 750
+            ],
+            [
+                'name' => 'Compre 3 ganhe 1',
+                'buy_quantity' => 3,
+                'get_quantity' => 1
+            ],
+            [
+                'name' => 'Compre 4 ganhe 10%',
+                'minimum_quantity' => 4,
+                'discount_percentage' => 10
+            ],
+            [
+                'name' => 'Compre 3 3,00',
+                'minimum_quantity' => 3,
+                'promotion_price' => 300
+            ]
+        ];
+
+        foreach ($rules as $rule) {
+            Rule::create($rule);
+        }
+    }
+}
