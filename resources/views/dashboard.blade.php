71386f59aa588ab1f3dc8100990f7abfb48470c4
João Dal Lago <50213926+jgdlago@users.noreply.github.com>
2024-02-22 01:52:18 -0300

    :sparkles: Feat: Adiciona dashboard com links de navegação
    Dashboard com conteúdo que pode ser exibido validando a autenticação.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Soluct Web Store') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+
+                    @guest()
+                        @section('content')
+                            <div class="flex flex-col items-center justify-center">
+                                <h1 class="text-3xl font-bold text-gray-800 mb-6">Bem-vindo a Soluct!</h1>
+
+                                <div class="grid grid-cols-2 gap-4">
+                                    <a href="{{ route('register') }}" class="btn btn-primary hover:text-blue-600 transition-colors duration-300">Crie uma conta</a>
+                                    <a href="{{ route('login') }}" class="btn btn-secondary hover:text-blue-600 transition-colors duration-300">Login</a>
+                                    <a href="{{ route('products.index') }}" class="btn btn-primary hover:text-blue-600 transition-colors duration-300">Ver Produtos</a>
+                                    <a href="{{ route('promotions.index') }}" class="btn btn-secondary hover:text-blue-600 transition-colors duration-300">Ver Promoções</a>
+                                </div>
+                            </div>
+                        @endsection
+                    @endguest
+
                 </div>
             </div>
         </div>
