6793747a459ac0d81fa2fe0ecc5279685dcb57b5
João Dal Lago <50213926+jgdlago@users.noreply.github.com>
2024-02-22 03:45:40 -0300

    :lipstick: Feat: Ajustes no layout de Rules
    Ajuste para facilitar a visualização das regras aplicadas.

--- resources/views/rules/index.blade.php
+++ resources/views/rules/index.blade.php
@@ -8,7 +8,6 @@
         <table class="table w-full">
             <thead>
             <tr>
-                <th class="px-4 py-2">ID</th>
                 <th class="px-4 py-2">Nome</th>
                 <th class="px-4 py-2">Comprar</th>
                 <th class="px-4 py-2">Ganhar</th>
@@ -21,15 +20,43 @@
             <tbody>
             @foreach ($rules as $rule)
                 <tr>
-                    <td class="border px-4 py-2">{{ $rule->id }}</td>
                     <td class="border px-4 py-2">{{ $rule->name }}</td>
-                    <td class="border px-4 py-2">{{ $rule->buy_quantity }}</td>
-                    <td class="border px-4 py-2">{{ $rule->get_quantity }}</td>
-                    <td class="border px-4 py-2">{{ $rule->minimum_quantity }}</td>
-                    <td class="border px-4 py-2">{{ $rule->promotion_price }}</td>
-                    <td class="border px-4 py-2">{{ $rule->discount_percentage }}</td>
                     <td class="border px-4 py-2">
-                        <a href="{{ route('rules.show', $rule->id) }}" class="btn btn-info">Ver</a>
+                        @if ($rule->buy_quantity === null || $rule->buy_quantity === 0)
+                            X
+                        @else
+                            {{ $rule->buy_quantity }}
+                        @endif
+                    </td>
+                    <td class="border px-4 py-2">
+                        @if ($rule->get_quantity === null || $rule->get_quantity === 0)
+                            X
+                        @else
+                            {{ $rule->get_quantity }}
+                        @endif
+                    </td>
+                    <td class="border px-4 py-2">
+                        @if ($rule->minimum_quantity === null || $rule->minimum_quantity === 0)
+                            X
+                        @else
+                            {{ $rule->minimum_quantity }}
+                        @endif
+                    </td>
+                    <td class="border px-4 py-2">
+                        @if ($rule->promotion_price === null || $rule->promotion_price === 0)
+                            X
+                        @else
+                            {{ $rule->promotion_price }}
+                        @endif
+                    </td>
+                    <td class="border px-4 py-2">
+                        @if ($rule->discount_percentage === null || $rule->discount_percentage === 0)
+                            X
+                        @else
+                            {{ $rule->discount_percentage }}
+                        @endif
+                    </td>
+                    <td class="border px-4 py-2">
                         <a href="{{ route('rules.edit', $rule->id) }}" class="btn btn-warning">Editar</a>
                         <form action="{{ route('rules.destroy', $rule->id) }}" method="POST" class="inline">
                             @csrf
@@ -41,5 +68,10 @@
             @endforeach
             </tbody>
         </table>
+
+        <div class="mt-4">
+            {{ $rules->links() }}
+        </div>
+
     </div>
 @endsection
